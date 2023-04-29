68b335e21a65a1ecfa813738f05ee47d4380c415
Siddharth <siddharthshah1102@gmail.com>
2023-04-29 14:05:12 +0530

    made class, simple error function

--- Lox.ts
+++ Lox.ts
@@ -1,34 +1,41 @@
-function executeCode(code: string) {
-    //to do - where code gets executed
-}
+export default class Lox {
 
-function runFile(filePath: string): void {
-    const code = Deno.readTextFileSync(filePath);
-    executeCode(code);
-}
+    static hadError = false;
 
-function runPrompt(): void {
-    while (true) {
-        const line = prompt(">");
-        if (line == null) {
-            break;
+    private static executeCode(code: string) {
+        //to do - where code gets executed
+        console.log(code);
+    }
+
+    private static runFile(filePath: string): void {
+        const code = Deno.readTextFileSync(filePath);
+        Lox.executeCode(code);
+    }
+
+    private static runPrompt(): void {
+        while (true) {
+            const line = prompt(">");
+            if (line == null) {
+                break;
+            }
+
+            Lox.executeCode(line);
         }
+    }
 
-        executeCode(line);
+    static main(args: string[]) {
+        if (args.length > 1) {
+            console.log("Usage: tslox [script]");
+        } else if (args.length === 1) {
+            Lox.runFile(args[0]);
+        } else {
+            Lox.runPrompt();
+        }
+    }
+
+
+    private static reportError(line: number, where: string, message: string): void {
+        console.log(`[line ${line}] Error ${where}: ${message}`);
+        Lox.hadError = true;
     }
 }
-
-function main(args: string[]) {
-    if (args.length > 1) {
-        console.log("Usage: tslox [script]");
-    
-    } else if (args.length === 1) {
-        runFile(args[0]);
-    
-    } else {
-        runPrompt();
-    }
-}
-
-
-main(Deno.args);
