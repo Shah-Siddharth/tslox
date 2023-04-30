ae35bfe74878a688b301ae1dbc0e580d779ea7d2
Siddharth <siddharthshah1102@gmail.com>
2023-04-30 12:56:41 +0530

    Token types and Token class

---
+++ Token.ts
@@ -0,0 +1,15 @@
+import TokenType from "./TokenType.ts";
+
+export default class Token {
+
+    constructor(
+        public type: TokenType, 
+        public lexeme: string,
+        public literal: string | number | undefined,
+        public line: number
+    ) {}
+
+    toString(): string {
+        return `${this.type} ${this.lexeme} ${this.literal}`;
+    }
+}
