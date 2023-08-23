798dda7eb1768fe7821e9750a0800b6d54899f8b
Siddharth <siddharthshah1102@gmail.com>
2023-08-23 19:10:08 +0530

    basic parser class and methods

---
+++ Parser.ts
@@ -0,0 +1,44 @@
+import Token from "./Token.ts";
+import TokenType from "./TokenType.ts";
+
+export default class Parser {
+  private current = 0;
+  private tokens: Token[];
+
+  constructor(tokens: Token[]) {
+    this.tokens = tokens;
+  }
+
+  private match(...types: TokenType[]) {
+    for (const type of types) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private check(type: TokenType): boolean {
+    if (this.isAtEnd()) return false;
+    return this.peek().type == type;
+  }
+
+  private advance(): Token {
+    if (!this.isAtEnd()) this.current++;
+    return this.previous();
+  }
+
+  private isAtEnd(): boolean {
+    return this.peek().type == TokenType.EOF;
+  }
+
+  private peek(): Token {
+    return this.tokens[this.current];
+  }
+
+  private previous(): Token {
+    return this.tokens[this.current - 1];
+  }
+}
