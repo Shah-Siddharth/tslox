d0744c8dac5f3cb956a1be2f0b8300d93a634bd3
Siddharth <siddharthshah1102@gmail.com>
2023-09-05 18:33:04 +0530

    astPrinter: cleanup unused code

--- AstPrinter.ts
+++ AstPrinter.ts
@@ -1,21 +1,6 @@
 import * as Expr from "./Expr.ts";
-import Token from "./Token.ts";
-import TokenType from "./TokenType.ts";
 
 export default class AstPrinter implements Expr.Visitor<string> {
-  static main() {
-    const expression = new Expr.Binary(
-      new Expr.Unary(
-        new Token(TokenType.MINUS, "-", null, 1),
-        new Expr.Literal(123),
-      ),
-      new Token(TokenType.STAR, "*", null, 1),
-      new Expr.Grouping(new Expr.Literal(45.67)),
-    );
-
-    console.log(new AstPrinter().print(expression));
-  }
-
   print(expr: Expr.Expr): string {
     return expr.accept(this);
   }
@@ -48,5 +33,3 @@ export default class AstPrinter implements Expr.Visitor<string> {
     return this.parenthisize(expr.operator.lexeme, expr.right);
   }
 }
-
-AstPrinter.main();
