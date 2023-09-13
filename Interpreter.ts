cbd169f303fe0b18d86f1a6f24663071fad8c3e3
Siddharth <siddharthshah1102@gmail.com>
2023-09-13 15:48:22 +0530

    interpreter: binary expressions

--- Interpreter.ts
+++ Interpreter.ts
@@ -20,11 +20,45 @@ export class Interpreter implements Visitor<LoxObject> {
       case TokenType.MINUS:
         return -(right as number);
     }
+
+    //unreachable
     return null;
   }
 
   visitBinaryExpr(expr: Binary): LoxObject {
-    return "temporary";
+    const left: LoxObject = this.evaluate(expr.left);
+    const right: LoxObject = this.evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case TokenType.GREATER:
+        return (left as number) > (right as number);
+      case TokenType.GREATER_EQUAL:
+        return (left as number) >= (right as number);
+      case TokenType.LESS:
+        return (left as number) < (right as number);
+      case TokenType.LESS_EQUAL:
+        return (left as number) <= (right as number);
+      case TokenType.EQUAL_EQUAL:
+        return this.isEqual(left, right);
+      case TokenType.BANG_EQUAL:
+        return !this.isEqual(left, right);
+      case TokenType.MINUS:
+        return (left as number) - (right as number);
+      case TokenType.SLASH:
+        return (left as number) / (right as number);
+      case TokenType.STAR:
+        return (left as number) * (right as number);
+      case TokenType.PLUS:
+        if (typeof left === "number" && typeof right === "number") {
+          return left + right;
+        }
+        if (typeof left === "string" && typeof right === "string") {
+          return left + right;
+        }
+    }
+
+    //unreachable
+    return null;
   }
 
   private evaluate(expr: Expr): LoxObject {
@@ -32,8 +66,15 @@ export class Interpreter implements Visitor<LoxObject> {
   }
 
   private isTruthy(object: LoxObject): boolean {
-    if (object == null) return false;
-    if (typeof object == "boolean") return object;
+    if (object === null) return false;
+    if (typeof object === "boolean") return object;
     return true;
+  }
+
+  private isEqual(a: LoxObject, b: LoxObject): boolean {
+    if (a === null && b === null) return true;
+    if (a === null) return false;
+
+    return a === b;
   }
 }
