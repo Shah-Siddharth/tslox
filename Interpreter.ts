9f41776371dab40d7bdafb77ee2f5af4e410ce43
Siddharth <siddharthshah1102@gmail.com>
2023-09-13 12:40:17 +0530

    interpreter: literals, parentheses and unary

--- Interpreter.ts
+++ Interpreter.ts
@@ -4,42 +4,36 @@ import TokenType from "./TokenType.ts";
 type LoxObject = number | string | boolean | null;
 
 export class Interpreter implements Visitor<LoxObject> {
-    visitLiteralExpr(expr: Literal): LoxObject {
-      return expr.value;
+  visitLiteralExpr(expr: Literal): LoxObject {
+    return expr.value;
+  }
+
+  visitGroupingExpr(expr: Grouping): LoxObject {
+    return this.evaluate(expr.expression);
+  }
+
+  visitUnaryExpr(expr: Unary): LoxObject {
+    const right: LoxObject = this.evaluate(expr.right);
+    switch (expr.operator.type) {
+      case TokenType.BANG:
+        return !this.isTruthy(right);
+      case TokenType.MINUS:
+        return -(right as number);
     }
+    return null;
+  }
 
-    visitGroupingExpr(expr: Grouping): LoxObject {
-      return this.evaluate(expr.expression);
-    }
+  visitBinaryExpr(expr: Binary): LoxObject {
+    return "temporary";
+  }
 
-    visitUnaryExpr(expr: Unary): LoxObject {
-      const right: LoxObject = this.evaluate(expr.right);
-      switch(expr.operator.type) {
-        case TokenType.BANG:
-          return !this.isTruthy(right);
-        case TokenType.MINUS:
-          return -(right as number);
-      }
-      return "temporary";
-    }
+  private evaluate(expr: Expr): LoxObject {
+    return expr.accept(this);
+  }
 
-    visitBinaryExpr(expr: Binary): LoxObject {
-      const left: LoxObject = this.evaluate(expr.left);
-      const right: LoxObject = this.evaluate(expr.right);
-
-      switch(expr.operator.type) {
-        case TokenType.MINUS:
-          return (left as number) - (right as number);
-      }
-
-      return "temporary";
-    }
-
-    private evaluate(expr: Expr): LoxObject {
-        return expr.accept(this);
-    }
-
-    private isTruthy(object: LoxObject): boolean {
-      return true;  //TODO: Implement this function
-    }
+  private isTruthy(object: LoxObject): boolean {
+    if (object == null) return false;
+    if (typeof object == "boolean") return object;
+    return true;
+  }
 }
