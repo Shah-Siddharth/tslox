bc36f325031c3c34f246076ce43fac79d5d09450
Siddharth Shah <siddharthshah1102@gmail.com>
2023-12-02 19:15:25 +0530

    resolver: resolve varStmt and variableExpr; deno fmt

--- Resolver.ts
+++ Resolver.ts
@@ -1,39 +1,81 @@
-import { Expr, Visitor as ExprVisitor } from "./Expr.ts";
+import { Expr, Variable, Visitor as ExprVisitor } from "./Expr.ts";
 import { Interpreter } from "./Interpreter.ts";
-import { Block, Stmt, Visitor as StmtVisitor } from "./Stmt.ts";
+import Lox from "./Lox.ts";
+import { Block, Stmt, Var, Visitor as StmtVisitor } from "./Stmt.ts";
+import Token from "./Token.ts";
 
-type SyntaxVisitor<RE, RS> = ExprVisitor<RE> & StmtVisitor<RS>
+type SyntaxVisitor<RE, RS> = ExprVisitor<RE> & StmtVisitor<RS>;
 
 export class Resolver implements SyntaxVisitor<void, void> {
-    private readonly interpreter: Interpreter;
-    
-    // stack to store scopes
-    private scopes: Map<string, boolean>[] = [];
+  private readonly interpreter: Interpreter;
 
-    constructor(interpreter: Interpreter) {
-        this.interpreter = interpreter;
+  // stack to store scopes
+  private scopes: Map<string, boolean>[] = [];
+
+  constructor(interpreter: Interpreter) {
+    this.interpreter = interpreter;
+  }
+
+  resolve(statements: Stmt[]): void;
+  resolve(stmt: Stmt | Expr): void;
+  resolve(target: Stmt[] | Stmt | Expr): void {
+    if (target instanceof Array) target.forEach((stmt) => this.resolve(stmt));
+    else target.accept(this);
+  }
+
+  beginScope(): void {
+    this.scopes.push(new Map());
+  }
+
+  endScope(): void {
+    this.scopes.pop();
+  }
+
+  private declare(name: Token): void {
+    if (this.scopes.length === 0) return;
+    const scope = this.scopes[this.scopes.length - 1];
+    scope.set(name.lexeme, false);
+  }
+
+  private define(name: Token): void {
+    if (this.scopes.length === 0) return;
+    const scope = this.scopes[this.scopes.length - 1];
+    scope.set(name.lexeme, true);
+  }
+
+  private resolveLocal(expr: Expr, name: Token): void {
+    for (let i = this.scopes.length - 1; i >= 0; i--) {
+      if (this.scopes[i].has(name.lexeme)) {
+        this.interpreter.resolve(expr, this.scopes.length - 1 - i);
+      }
+    }
+  }
+
+  visitBlockStmt(stmt: Block): void {
+    this.beginScope();
+    this.resolve(stmt.statements);
+    this.endScope();
+  }
+
+  visitVarStmt(stmt: Var): void {
+    this.declare(stmt.name);
+    if (stmt.initializer !== null) {
+      this.resolve(stmt.initializer);
     }
 
-    resolve(statements: Stmt[]): void
-    resolve(stmt: Stmt | Expr): void
-    resolve(target: Stmt[] | Stmt | Expr): void {
-        if (target instanceof Array) target.forEach((stmt) => this.resolve(stmt));
-        else target.accept(this);
+    this.define(stmt.name);
+  }
+
+  visitVariableExpr(expr: Variable): void {
+    if (this.scopes.length > 0) {
+      if (!this.scopes[this.scopes.length - 1].get(expr.name.lexeme)) {
+        Lox.error(
+          expr.name,
+          "Can't read local variable in its own initializer.",
+        );
+      }
     }
 
-    beginScope(): void {
-        this.scopes.push(new Map());
-    }
-
-    endScope(): void {
-        this.scopes.pop();
-    }
-
-    visitBlockStmt(stmt: Block): void {
-      this.beginScope();
-      this.resolve(stmt.statements);
-      this.endScope();
-    }
-
-    
+    this.resolveLocal(expr, expr.name);
+  }
 }
